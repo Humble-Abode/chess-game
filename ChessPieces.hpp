125e3a02813191b6a584c1642e536e40dbe0de48
NathanBarquira <nathanbarquira@gmail.com>
2022-07-07 16:45:01 -0700

    rules for pawn

--- ChessPieces.hpp
+++ ChessPieces.hpp
@@ -1,5 +1,9 @@
 // This will be the modules that contain the chess pieces
 
+// Class for pawn.
+// Can only move forward, can only capture diagonal.
+// If the pawn hasn't moved, it can move forward 2 spaces.
+// Once it reaches the end, it can promote to ANY piece.
 class Pawn
 {
     void move();
