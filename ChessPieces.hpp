52e9da48af4fd800b6a7c3c990d831ee2a20540e
NathanBarquira <nathanbarquira@gmail.com>
2022-07-21 00:23:12 -0700

    updated structure, wrote future ideas

--- ChessPieces.hpp
+++ ChessPieces.hpp
@@ -1,26 +1,38 @@
 // This will be the modules that contain the chess pieces
 
+#include "GameBoard.hpp"
+
+// Class for piece of the board.
+// Will be inherited.
+class Piece
+{
+
+    // will hold a reference to the actual board, where
+    // piece moves will change the gamestate.
+    GameBoard& board;
+};
+
 // Class for pawn.
 // Can only move forward, can only capture diagonal.
 // If the pawn hasn't moved, it can move forward 2 spaces.
 // Once it reaches the end, it can promote to ANY piece.
-class Pawn 
+class Pawn : Piece
 {
     void move();
+    
 };
 
 // Class for knight
 // Can only move in L-shapes
 // Can jump over pieces
-
-class Knight
+class Knight : Piece
 {
     void move();
 };
 
 // Class for bishop
 // Can only move and capture diagonally
-class Bishop
+class Bishop : Piece
 {
     void move();
 };
@@ -28,14 +40,14 @@ class Bishop
 // Class for rook
 // Can only move and capture horizontally
 // Can "castle" with king
-class Rook
+class Rook : Piece
 {
     void move();
 };
 
 // Class for queen
 // Caan move and capture horizontally and vertically
-class Queen
+class Queen : Piece
 {
     void move();
 };
@@ -44,7 +56,7 @@ class Queen
 // Can move and capture horizontally and vertically, but only one square
 // Cannot move itself into a square that would get itself captured on the next move
 // Can "Castle" with the rook
-class King
+class King : Piece
 {
     void move();
 };
---
+++ GameBoard.hpp
@@ -0,0 +1,11 @@
+// Class for the game board.
+// Will contain a map as the underlying data structure,
+// where each vertex contains a piece information
+// and edges connecting vertices are determined by
+// if a piece can access that vertex (valid move)
+
+
+class GameBoard
+{
+
+};
