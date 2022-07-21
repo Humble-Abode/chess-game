b54f8ca16ca36305813faa99c92329cd6770f85c
NathanBarquira <nathanbarquira@gmail.com>
2022-07-21 02:56:06 -0700

    added spacing

--- tester.cpp
+++ tester.cpp
@@ -7,9 +7,14 @@
 
 int main()
 {
+    print();
     print("testing construction of gameboard");
     GameBoard testBoard;
 
+    print();
     print("testing construction for piece class");
     Piece testPiece(testBoard);
+
+    print();
+    return 0;
 }
